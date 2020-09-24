f4a7ac056f2c6c0c0e484103d86b2d23d10a82b0
Michael Nikitenko <xm4dn355x@gmail.com>
2020-09-24 11:46:25 +0500

    created event loop for timer

--- sandbox.py
+++ sandbox.py
@@ -11,6 +11,7 @@
 
 from datetime import datetime
 from datetime import timedelta
+from time import sleep
 import threading
 import psycopg2
 from psycopg2.extras import DictCursor
@@ -23,19 +24,19 @@ cursor = conn.cursor(cursor_factory=DictCursor)
 cursor.execute("""SET TIMEZONE='Asia/Yekaterinburg';""")
 
 
-def make_alert(id, timer_delay, chat_id, user, message):
+def make_alert(id, timer_delay, chat_id, message):
     """Создает событие оповещения"""
-    t = threading.Timer(timer_delay, send_alert, args=(id, chat_id, user, message,))
+    t = threading.Timer(timer_delay, send_alert, args=(id, chat_id, message,))
     t.start()
-    return t.name
+    return t
 
 
-def send_alert(id, chat_id, user, message):
-    """Мок функция для проверки работы оповещений"""
-    print(f'alert: {chat_id} {user} {message} {datetime.now()}')
-    thread_name = threading.current_thread().name
-    pop_allert(id, thread_name)
-
+def send_alert(id, chat_id, message):
+    """Отправка оповещения"""
+    print(f'alert: {chat_id} {message} {datetime.now()}')
+    pop_allert(id)
+    change_alert_status_in_db(id)
+    print(ALERTS_LIST)
 
 
 def calculate_timer_delay(alert_time):
@@ -47,16 +48,23 @@ def calculate_timer_delay(alert_time):
     return res
 
 
-def add_alert(id, time, chat_id, user, message):
+def add_alert(id, time, chat_id, message):
     """Создает событие оповещения и добавляет его в список"""
     timer_delay = calculate_timer_delay(time)
     timer_delay = float(timer_delay.seconds)
-    thread_name = make_alert(id, timer_delay, chat_id, user, message)
-    ALERTS_LIST.append({'id': id, 'thread_name': str(thread_name)})
+    thread = make_alert(id, timer_delay, chat_id, message)
+    ALERTS_LIST.append({'id': id, 'thread': thread})
 
 
-def pop_allert(id, thread_name):
-    ALERTS_LIST.remove({'id': id, 'thread_name': thread_name})
+def pop_allert(id):
+    """Удаляет событие оповещения из списка"""
+    for alert in ALERTS_LIST:
+        if alert['id'] == id:
+            ALERTS_LIST.remove(alert)
+
+
+def generate_message(username):
+    return f"@{username} Обратный отсчет окончен. Отчитайтесь пожалуйста"
 
 
 def get_alerts_list_from_db():
@@ -64,19 +72,42 @@ def get_alerts_list_from_db():
     return cursor.fetchall()
 
 
-if __name__ == '__main__':
-    alerts_list = get_alerts_list_from_db()
-    for alert in alerts_list:
-        add_alert(
-            id=alert['id'],
-            time=alert['time'],
-            chat_id=alert['chat_id'],
-            user=alert['username'],
-            message='Hello Bitch!'
-        )
+def change_alert_status_in_db(id):
+    cursor.execute(f"""UPDATE alerts SET status = TRUE WHERE id = {int(id)};""")
+    conn.commit()
 
-    # TODO: реализовать выключение потоков скорей всего добавлять в список потоки и потом прогоняться по имено и закрывать
-    # TODO: ебануть управление через бота
+
+def run_timers_event_loop():
+    """Запускает ивент луп"""
+    x = threading.Thread(target=timers_event_loop)
+    return x
+
+
+def timers_event_loop():
+    while True:
+        alerts_list = get_alerts_list_from_db()
+        for alert in alerts_list:
+            add_flag = True
+            for timer in ALERTS_LIST:
+                if int(timer['id']) == alert['id']:
+                    add_flag = False
+            if add_flag == True:
+                add_alert(
+                    id=alert['id'],
+                    time=alert['time'],
+                    chat_id=alert['chat_id'],
+                    message=generate_message(alert['username'])
+                )
+            else:
+                print('Alert in list')
+        print(ALERTS_LIST)
+        sleep(5)
+
+
+
+if __name__ == '__main__':
+    event_loop = run_timers_event_loop()
+    event_loop.start()
 
 
 # id
