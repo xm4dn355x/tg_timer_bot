7d3f2b944cb1650e3241b60e451064daa9965996
Michael Nikitenko <xm4dn355x@gmail.com>
2020-09-24 18:20:14 +0500

    PEP8

--- sandbox.py
+++ sandbox.py
@@ -17,8 +17,10 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 
+# Globals
 ALERTS_LIST = []
 
+# DB Connection
 conn = psycopg2.connect(dbname='alerts_bot', user='alerts_bot', password='alerts_bot', host='localhost')
 cursor = conn.cursor(cursor_factory=DictCursor)
 cursor.execute("""SET TIMEZONE='Asia/Yekaterinburg';""")
@@ -31,12 +33,11 @@ def make_alert(id, timer_delay, chat_id, message):
     return t
 
 
-def send_alert(id, chat_id, message):
+def send_alert(id, chat_id, message):   # TODO: Переделать замоканную функцию под метод бота.
     """Отправка оповещения"""
     print(f'alert: {chat_id} {message} {datetime.now()}')
     pop_allert(id)
     change_alert_status_in_db(id)
-    print(ALERTS_LIST)
 
 
 def calculate_timer_delay(alert_time):
@@ -44,7 +45,6 @@ def calculate_timer_delay(alert_time):
     res = alert_time.replace(tzinfo=None) - datetime.now().replace(tzinfo=None)
     if res < timedelta(seconds=0):
         res = timedelta(seconds=5)
-    print(res)
     return res
 
 
@@ -64,26 +64,30 @@ def pop_allert(id):
 
 
 def generate_message(username):
+    """Генерирует сообщение, которое нужно отправить в чат"""
     return f"@{username} Обратный отсчет окончен. Отчитайтесь пожалуйста"
 
 
 def get_alerts_list_from_db():
+    """Получает список всех неотработавших таймеров"""
     cursor.execute("""SELECT id, chat_id, username, time FROM alerts WHERE status = FALSE""")
     return cursor.fetchall()
 
 
 def change_alert_status_in_db(id):
+    """Изменяет статус таймера на отработанный в БД"""
     cursor.execute(f"""UPDATE alerts SET status = TRUE WHERE id = {int(id)};""")
     conn.commit()
 
 
 def run_timers_event_loop():
-    """Запускает ивент луп"""
+    """Запускает ивент луп с таймерами"""
     x = threading.Thread(target=timers_event_loop)
     return x
 
 
 def timers_event_loop():
+    """Event loop для обработки тасков с таймерами"""
     while True:
         alerts_list = get_alerts_list_from_db()
         for alert in alerts_list:
@@ -91,27 +95,16 @@ def timers_event_loop():
             for timer in ALERTS_LIST:
                 if int(timer['id']) == alert['id']:
                     add_flag = False
-            if add_flag == True:
+            if add_flag:
                 add_alert(
                     id=alert['id'],
                     time=alert['time'],
                     chat_id=alert['chat_id'],
                     message=generate_message(alert['username'])
                 )
-            else:
-                print('Alert in list')
-        print(ALERTS_LIST)
         sleep(5)
-
 
 
 if __name__ == '__main__':
     event_loop = run_timers_event_loop()
     event_loop.start()
-
-
-# id
-# chat_id
-# user
-# time
-# status
